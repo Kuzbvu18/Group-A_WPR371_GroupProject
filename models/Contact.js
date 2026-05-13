18ba80226c4c40c676c4660d9233e4d3a21f4943
denise-apple <602525@student.belgiumcampus.ac.za>
2026-05-13 10:24:30 +0200

    Refactor Contact schema for improved readability and maintainability

--- models/Contact.js
+++ models/Contact.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose');
 
 const contactSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  subject: { type: String, required: true },
-  message: { type: String, required: true },
-  status: { type: String, enum: ['pending', 'resolved'], default: 'pending' }
-}, { timestamps: true });
+  name: { type: String,
+          required: true
+        },
+  email: { type: String,
+           required: true
+         },
+  subject: { type: String,
+             required: true
+           },
+  message: { type: String,
+             required: true 
+           },
+  status: { type: String,
+            enum: ['pending', 'resolved'],
+            default: 'pending'
+          },
+  category: { type: String,
+              enum: ['General Entry', 'Ticket Support', 'Event Hosting', 'Other'],
+              default: 'General Entry'
+  }    // purpose of category: to assist admins filter enquires
+}, 
+{ timestamps: true });
 
 module.exports = mongoose.model('Contact', contactSchema);
+
