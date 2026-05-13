4b07f8b034573dd54357d91100b9c0ef9e12b573
denise-apple <602525@student.belgiumcampus.ac.za>
2026-05-13 16:58:39 +0200

    Refactor email field formatting in Contact schema for consistency

--- models/Contact.js
+++ models/Contact.js
@@ -4,12 +4,12 @@ const contactSchema = new mongoose.Schema({
   name: { type: String,
           required: true
         },
-  email: { type: String,
-           required: true
-         },
+  email:{ type: String,
+          required: true
+        },
   subject: { type: String,
              required: true
-           },
+            },
   message: { type: String,
              required: true 
            },
